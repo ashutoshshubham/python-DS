278664ab05dbe799a43251cf121de021c7649a13
ashutoshshubham <ashutoshshubham29@gmail.com>
2022-07-25 17:39:42 +0530

    work

---
+++ average.py
@@ -0,0 +1,19 @@
+"""n = int(input("enter number of elements "))
+a = []
+for i in range (0,n):
+    b = int(input())
+    a.append(b)
+add = sum(a)
+avg = add/n
+
+print(avg)"""
+
+
+n1 = 1
+n2 = 2
+n3 = 3
+n4 = 4 
+n5 = 5
+
+avg = (n1+n2+n3+n4+n5)/5
+print(avg)
