200718423d509177f52a3eec0bcafb6dbda9b225
ashutoshshubham <ashutoshshubham29@gmail.com>
2022-08-02 12:08:09 +0530

    basics

---
+++ data structures/string2.py
@@ -0,0 +1,13 @@
+# for i in range(1000,1600):
+#     print(chr(i),end = ' | ')
+
+# y = ord('`')
+# print(y)
+
+# print(len('131311644'))
+
+w1 = "this"
+w2 = "is"
+w3 = "amazing"
+
+print((w1 + ' ' + w2 + ' ' + w3 + ' ') * 3)
